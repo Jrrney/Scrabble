10e74ecc7d0606b516cfba15c86a217bef9caffa
Adam Pirog <pirog.adam@gmail.com>
2020-04-22 15:39:23 +0200

    Empty board endpoint

---
+++ Scrabble.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+import csv
+import random
+import string
+import time
+
+import pickle
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+
+@app.route("/", methods=['POST', 'GET'])
+def startGame():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template('empty_board.html')
+
+
+if (__name__ == "__main__"):
+    app.run()
